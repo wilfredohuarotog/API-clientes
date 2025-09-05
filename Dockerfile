32dc40ea68fe8c21038b8c3e0e8b870503963ee5
wilfredohuarotog <wilfredohuarotogabino@gmail.com>
2025-09-05 13:44:59 -0500

    add Docker support with Dockerfile and docker-compose

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM maven:3.8.5-openjdk-17-slim AS builder
+
+WORKDIR /app
+
+COPY pom.xml /app
+
+RUN mvn dependency:go-offline
+
+COPY ./src /app/src
+
+RUN mvn clean install -DskipTests
+
+FROM eclipse-temurin:17-jdk-alpine-3.22
+
+COPY --from=builder /app/target/*.jar app_clientes.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","app_clientes.jar"]
--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into cliente (nombre, apellido, correo, edad) values ("Wilfredo", "Huaroto", "will@gmail.com",26);
-insert into cliente (nombre, apellido, correo, edad) values ("Rocio", "Correa", "rc@gmail.com",28);
-insert into cliente (nombre, apellido, correo, edad) values ("Juan", "Vela", "juanv@gmail.com",21);
-insert into cliente (nombre, apellido, correo, edad) values ("Ricado", "Gomez", "rgomez@gmail.com",19);
